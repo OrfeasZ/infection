79c5aa561174b9048580f6d9869b3aab69ef3431
Orfeas Zafeiris <orfeaz@gmail.com>
2020-09-28 15:10:42 +0300

    Adjust default settings

--- ext/Server/config.lua
+++ ext/Server/config.lua
@@ -1,7 +1,7 @@
 Config = {
-	TimeLimit = 0, -- Round time limit in seconds for the main game phase.
+	TimeLimit = 300, -- Round time limit in seconds for the main game phase.
 	ExtractionTime = 120, -- Amount of time given to players to get to the extraction point.
-	PreInfectionTime = 1, -- Amount of time in seconds until the first player gets infected.
+	PreInfectionTime = 20, -- Amount of time in seconds until the first player gets infected.
 	PostRoundTime = 30, -- Amount of time to wait after the end of a round until a new round is started.
-	MinPlayers = 2, -- Minimum amount of players required to start a game.
+	MinPlayers = 3, -- Minimum amount of players required to start a game.
 }
